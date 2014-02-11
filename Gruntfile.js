83948fe979ee3f4ada519fd5db4e9bac3cea0ec4
Tanmay Patel <me@tanmaypatel.com>
2014-02-11 15:57:07 +0530

    Moved source files to "src/" folder and distribution files are outside "src/"

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,122 +2,141 @@ module.exports = function(grunt)
 {
 
 	grunt.initConfig({
-						 less: {
-							 options: {
-								 report: 'min'
-							 },
-							 development: {
-								 options: {
-									 dumpLineNumbers: 'comments'
-								 },
-								 files: {
-									 'styles/dist/style.css': ['styles/src/style.less']
-								 }
-							 },
-							 dist: {
-								 options: {
-									 yuicompress: true,
-									 cleancss: true
-								 },
-								 files: {
-									 'styles/dist/style.css': ['styles/src/style.less']
-								 }
-							 }
-						 },
+		config: {
+			srcLocation: 'src',
+			distLocation: '.'
+		},
+		less: {
+			options: {
+				report: 'min'
+			},
+			development: {
+				options: {
+					dumpLineNumbers: 'comments'
+				},
+				files: {
+					'<%= config.srcLocation %>/styles/dist/style.css': [ '<%= config.srcLocation %>/styles/src/style.less' ]
+				}
+			},
+			dist: {
+				options: {
+					yuicompress: true,
+					cleancss: true
+				},
+				files: {
+					'<%= config.distLocation %>/styles/dist/style.css': [ '<%= config.srcLocation %>/styles/src/style.less' ]
+				}
+			}
+		},
 
-						 jshint: {
-							 options: {
-								 smarttabs: true
-							 },
-							 development: {
-								 files: {
-									 src: ['Gruntfile.js',
-										   'scripts/src/script.js']
-								 }
-							 }
-						 },
+		jshint: {
+			options: {
+				smarttabs: true
+			},
+			development: {
+				files: {
+					src: [ 'Gruntfile.js', '<%= config.srcLocation %>/scripts/src/script.js' ]
+				}
+			}
+		},
 
-						 uglify: {
-							 components: {
-								 files: {
-									 'scripts/dist/components.min.js': ['components/modernizr/modernizr.js',
-									                                    'components/jquery/jquery-1.11.0.min.js',
-									                                    'components/jquery/jquery-migrate-1.2.1.min.js',
-									                                    'components/bootstrap/js/bootstrap.min.js']
-								 }
-							 },
-							 dist: {
-								 files: {
-									 'scripts/dist/script.min.js': ['scripts/src/script.js']
-								 }
-							 }
-						 },
+		uglify: {
+			components: {
+				files: {
+					'<%= config.srcLocation %>/scripts/dist/components.min.js': [ '<%= config.srcLocation %>/components/modernizr/modernizr.js', '<%= config.srcLocation %>/components/jquery/jquery-1.11.0.min.js', '<%= config.srcLocation %>/components/jquery/jquery-migrate-1.2.1.min.js', '<%= config.srcLocation %>/components/bootstrap/js/bootstrap.min.js' ]
+				}
+			},
+			dist: {
+				files: {
+					'<%= config.distLocation %>/scripts/dist/script.min.js': [ '<%= config.srcLocation %>/scripts/src/script.js' ]
+				}
+			}
+		},
 
-						 uncss: {
-							 options: {
-								 ignore: [/\.fixedNavigation*/]
-							 },
-							 dist: {
-								 files: {
-									 'styles/dist/style.tidy.css': ['index.src.html']
-								 }
-							 }
-						 },
+		uncss: {
+			options: {
+				ignore: [ /\.fixedNavigation*/ ]
+			},
+			dist: {
+				files: {
+					'<%= config.distLocation %>/styles/dist/style.tidy.css': [ '<%= config.srcLocation %>/index.html' ]
+				}
+			}
+		},
 
-						 cssmin: {
-							 options: {
-								 keepSpecialComments: 0,
-								 report: 'min'
-							 },
-							 dist: {
-								 files: {
-									 'styles/dist/style.css': ['styles/dist/style.tidy.css']
-								 }
-							 }
-						 },
+		cssmin: {
+			options: {
+				keepSpecialComments: 0,
+				report: 'min'
+			},
+			dist: {
+				files: {
+					'<%= config.distLocation %>/styles/dist/style.css': [ '<%= config.distLocation %>/styles/dist/style.tidy.css' ]
+				}
+			}
+		},
 
-						 copy: {
-							 development: {
-								 files: [
-									 {src: ['scripts/src/script.js'], dest: 'scripts/dist/script.min.js'}
-								 ]
-							 }
-						 },
+		copy: {
+			development: {
+				files: [ {
+					src: [ '<%= config.srcLocation %>/scripts/src/script.js' ],
+					dest: '<%= config.srcLocation %>/scripts/dist/script.min.js'
+				} ]
+			},
+			dist: {
+				files: [ {
+					src: [ '<%= config.srcLocation %>/scripts/dist/components.min.js' ],
+					dest: '<%= config.distLocation %>/scripts/dist/components.min.js'
+				}, {
+					src: [ '<%= config.srcLocation %>/assets/Tanmay Patel.pdf' ],
+					dest: '<%= config.distLocation %>/assets/Tanmay Patel.pdf'
+				}, {
+					cwd: '<%= config.srcLocation %>/styles/images/',
+					src: '**',
+					dest: '<%= config.distLocation %>/styles/images/',
+					expand: true
+				}, {
+					cwd: '<%= config.srcLocation %>/styles/fonts/',
+					src: '**',
+					dest: '<%= config.distLocation %>/styles/fonts/',
+					expand: true
+				}]
+			}
+		},
 
-						 clean: {
-							 dist_after: {
-								 src: ['styles/dist/style.temp.css', 'styles/dist/style.tidy.css']
-							 }
-						 },
+		clean: {
+			dist_after: {
+				src: [ '<%= config.distLocation %>/styles/dist/style.tidy.css' ]
+			}
+		},
 
-						 watch: {
-							 options: {
-								 livereload: true
-							 },
-							 styles: {
-								 files: 'styles/src/**/*.less',
-								 tasks: ['less:development']
-							 },
-							 scripts: {
-								 files: 'scripts/src/**/*.js',
-								 tasks: ['copy:development']
-							 }
-						 },
-						 
-						 processhtml: {
-							 options: {
-								 data: {
-									 message: 'Preparing Distribution File!'
-								 }
-							 },
-							 dist: {
-								 files: {
-									 'index.html': ['index.src.html']
-								 }
-							 }
-						 }
-					 }
-	);
+		watch: {
+			options: {
+				livereload: true
+			},
+			styles: {
+				files: '<%= config.srcLocation %>/styles/src/**/*.less',
+				tasks: [ 'less:development' ]
+			},
+			scripts: {
+				files: '<%= config.srcLocation %>/scripts/src/**/*.js',
+				tasks: [ 'copy:development' ]
+			}
+		},
+
+		processhtml: {
+			options: {
+				data: {
+					message: 'Preparing Distribution File!'
+				}
+			},
+			dist: {
+				files: {
+					'<%= config.distLocation %>/index.html': [ '<%= config.srcLocation %>/index.html' ]
+				}
+			}
+		}
+	});
 
 	// Load Required Grunt Plug-in(s)
 	grunt.loadNpmTasks('grunt-contrib-less');
@@ -132,6 +151,6 @@ module.exports = function(grunt)
 	grunt.loadNpmTasks('grunt-processhtml');
 
 	// Default Task(s)
-	grunt.registerTask('default', ['less:development', 'jshint:development', 'uglify:components', 'copy:development']);
-	grunt.registerTask('dist', ['less:dist', 'jshint:development', 'uglify:components', 'uglify:dist', 'uncss:dist', 'cssmin:dist', 'clean:dist_after', 'processhtml:dist']);
+	grunt.registerTask('default', [ 'less:development', 'jshint:development', 'uglify:components', 'copy:development' ]);
+	grunt.registerTask('dist', [ 'less:dist', 'jshint:development', 'uglify:components', 'uglify:dist', 'copy:dist', 'uncss:dist', 'cssmin:dist', 'clean:dist_after', 'processhtml:dist' ]);
 };
